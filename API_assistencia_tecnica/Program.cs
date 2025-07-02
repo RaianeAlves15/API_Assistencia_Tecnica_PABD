16f13f57c2bd874c126f4183cc9fd9cc4a24db56
RaianeAlves15 <raianealvesmacedo15@gmail.com>
2025-07-01 22:57:41 -0300

    Configuração para vincular o banco de dados

--- API_assistencia_tecnica/Program.cs
+++ API_assistencia_tecnica/Program.cs
@@ -1,15 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using API_assistencia_tecnica.DataContexts;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona o DbContext com MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 36)) // use sua versão do MySQL
+    )
+);
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure o pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +27,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
