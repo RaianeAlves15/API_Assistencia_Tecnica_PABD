034d210c0f2ef23ee784f8029986a4d6fdd82f39
MIRIAM LENZI <160069169+MiriamLenzi@users.noreply.github.com>
2025-07-08 21:32:18 -0400

    Alteração DataContexts
    Alteração arquivo MapperProfile

---
+++ API_assistencia_tecnica/DataContexts/MapperProfile.cs
@@ -0,0 +1,41 @@
+using API_assistencia_tecnica.Dtos;
+using API_assistencia_tecnica.Models;
+//using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace API_assistencia_tecnica.DataContexts
+{
+   /* public class MapperProfile : Profile
+    {
+     //   public MapperProfile()
+        {
+            // Cliente
+          //  CreateMap<ClienteDto, Cliente>();
+            //CreateMap<Cliente, ClienteDto>();
+
+            // Equipamento
+            //CreateMap<EquipamentoDto, Equipamento>();
+           // CreateMap<Equipamento, EquipamentoDto>();
+
+            // Fornecedor
+           // CreateMap<FornecedorDto, Fornecedor>();
+            //CreateMap<Fornecedor, FornecedorDto>();
+
+            // Peça
+          //  CreateMap<PecaDto, Peca>();
+           // CreateMap<Peca, PecaDto>();
+
+            // Orçamento
+           // CreateMap<OrcamentoDto, Orcamento>();
+            //CreateMap<Orcamento, OrcamentoDto>();
+
+            // Reparo
+           // CreateMap<ReparoDto, Reparo>();
+           // CreateMap<Reparo, ReparoDto>();
+        }
+    }*/
+}
+
+
+
+// adicionar no program builder.Services.AddAutoMapper(typeof(MapperProfile));
